05aac036fe92bd9248b3b9b9c64505c89f5f2033
maximmft <mmaufront@gmail.com>
2024-04-02 14:46:09 +0200

    update CSS

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -5,12 +5,16 @@ function WeatherCard ({city, temperature, temperatureMax, temperatureMin, sky, h
 return (
     <section className='weatherCard'>
         <h2>{city}</h2>
-        <p id="temperature">{temperature}</p>
-        <p> {sky}</p>
-        <div className="temperatureMaxMin">
-            <p>{temperatureMax} {temperatureMin}</p>
+        <div className='temperatures'>
+            <p id="temperature">{temperature}</p>
+            <div id="temperatureMaxMin">
+                <p>Max: {temperatureMax} </p>
+                <p>Min: {temperatureMin}</p>
+            </div>
         </div>
         <p> {sky}</p>
+        <div className="temperatureMaxMin">
+        </div>
         <p> {humidity}</p>
     </section>
 )}
